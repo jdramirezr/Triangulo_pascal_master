4db3e2cb33be0840e686ace5a64db1db5ab86d1f
jdramirezr <jdramirezr686@gmail.com>
2019-01-15 14:21:52 -0500

    triangulo ppascal version 2

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.ListBox2 = New System.Windows.Forms.ListBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.ListBox3 = New System.Windows.Forms.ListBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtfibonaci = New System.Windows.Forms.TextBox
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label7 = New System.Windows.Forms.Label
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(265, 469)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Construir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(194, 453)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(29, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(591, 403)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(383, 469)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Salir"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(762, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(186, 160)
+        Me.ListBox1.TabIndex = 5
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(762, 194)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(186, 160)
+        Me.ListBox2.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(626, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Serie de Fibonacci"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(627, 266)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Fractales de Sierpinski"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(762, 384)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(73, 108)
+        Me.ListBox3.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(627, 416)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Sumatoria diagonal"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Izquierda", "Derecha"})
+        Me.ComboBox1.Location = New System.Drawing.Point(629, 453)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(95, 21)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(841, 384)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Suma:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(910, 402)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 13
+        '
+        'txtfibonaci
+        '
+        Me.txtfibonaci.Location = New System.Drawing.Point(194, 486)
+        Me.txtfibonaci.Name = "txtfibonaci"
+        Me.txtfibonaci.Size = New System.Drawing.Size(51, 20)
+        Me.txtfibonaci.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(88, 456)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "numero de filas"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(88, 486)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(106, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "numero de fibonacci "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1009, 518)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtfibonaci)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtfibonaci As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,354 @@
+﻿Public Class Form1
+
+
+
+    Public Function par(ByVal n)
+        Dim numero As Integer
+        Dim npar As Integer
+
+        numero = n
+
+        If numero Mod 2 = 0 Then
+            npar = n + 2
+            npar = npar / 2
+        Else
+            npar = n + 1
+            npar = npar / 2
+        End If
+
+        Return (npar)
+
+    End Function
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        DataGridView1.Columns.Clear()
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        ListBox3.Items.Clear()
+
+
+        'Try
+
+
+        Dim n As Integer
+        Dim fila As Integer
+        Dim columna As Integer
+        Dim i As Integer
+        Dim j As Integer
+        Dim inicio As Integer
+
+        'Button3.Enabled = True
+        n = TextBox1.Text
+        fila = n + 1
+        columna = n + fila
+        fila = fila - 1
+        columna = columna - 1
+        For x = 0 To columna
+            DataGridView1.Columns.Add(x, "")
+            DataGridView1.Columns(x).Width = 30
+        Next
+        For x = 0 To fila
+            DataGridView1.Rows.Add(x, "")
+        Next
+        Dim fibonacci(fila) As Double
+        'fibonacci(0) = 1
+        'fibonacci(1) = 1
+
+        'Llenar matriz de espacios en blanco
+
+        For i = 0 To columna
+            For j = 0 To fila
+                DataGridView1(i, j).Value = ""
+            Next
+        Next
+
+        Dim q As Integer
+        Dim anterior As Integer
+        Dim despues As Integer
+
+        inicio = columna / 2
+        q = 0
+        anterior = inicio - q
+        despues = inicio + q
+
+        Dim paranterior As Integer
+        Dim pardespues As Integer
+        paranterior = anterior + 1
+        pardespues = despues + 1
+
+        Dim fianterior As Integer
+        Dim fidespues As Integer
+        Dim iteracion As Integer
+
+
+        ' TRIANGULO DE PASCAL
+
+        Dim k As Integer
+        k = 0
+        Dim suma As Double
+        suma = 0
+        fianterior = anterior + 1
+        fidespues = despues + 1
+
+        For i = 0 To fila
+            anterior = inicio - q
+            despues = inicio + q
+
+
+            DataGridView1(anterior, i).Value = 1
+            DataGridView1(despues, i).Value = 1
+            q = q + 1
+
+            For j = anterior To despues Step 2
+
+                If j > anterior And j < despues Then
+
+                    ' Sumar e imprimir vlores para completar el triangulo de pascal
+
+                    DataGridView1(j, i).Value = DataGridView1(j - 1, i - 1).Value + DataGridView1(j + 1, i - 1).Value
+
+                End If
+            Next
+
+        Next
+
+        ' SERIE DE FIBONACCI
+        Dim listado As Integer
+        Dim g As Integer
+        g = 0
+        Dim v As Integer
+        v = 2
+        Dim t As Integer
+        Dim colsum As Integer
+        Dim filsum As Integer
+
+        q = 0
+        columna = columna - 3
+
+         Dim valofibonaci As Integer
+        If txtfibonaci.Text <> "" Then
+            valofibonaci = txtfibonaci.Text
+
+        End If
+        For i = 0 To fila
+            fianterior = inicio - q
+            fidespues = inicio + q
+            q = q + 1
+
+            ' Llamamos funcion par, para saber cuantas repeticiones realizar al momento de sumar, formula # iteraciones, SIGUIENTE PAR DIVIDIDO ENTRE DOS
+
+            iteracion = par(i)
+
+
+            For k = fianterior To fidespues Step 2
+                g = k
+
+                If k <= fidespues And i > 1 Then
+                    If g < columna Then
+                        colsum = g
+                        filsum = i
+
+
+                        For t = 0 To iteracion - 1
+                            suma = suma + DataGridView1(colsum, filsum).Value
+                            ListBox1.Items.Add(DataGridView1(colsum, filsum).Value)
+                            colsum = colsum + 3
+                            filsum = filsum - 1
+
+
+                        Next
+
+                        ' Asignamos al vector de fibonacci la suma
+
+                        fibonacci(v) = suma
+
+                        v = v + 1
+                        suma = 0
+
+
+                        Exit For
+
+                    End If
+                End If
+            Next
+
+        Next
+
+
+        Dim y As Integer
+        y = 0
+
+
+
+
+
+
+
+        'If txtfibonaci.Text <> "" Then
+        '    valofibonaci = txtfibonaci.Text
+
+        'End If
+        If valofibonaci > n Then
+            MsgBox("el numero de fibonaci no puede ser mayor al numero de filas ", MsgBoxStyle.Critical, "error")
+            txtfibonaci.Text = ""
+            txtfibonaci.Focus()
+        End If
+        For y = 0 To fila
+            If txtfibonaci.Text = "" Then
+                ListBox1.Items.Add("")
+            ElseIf valofibonaci = y Then
+
+                ListBox1.Items.Add("numero de Fibonacci " & y & "  =   " & fibonacci(y))
+                'DataGridView1.Rows(i).Cells(y).Style.BackColor = Drawing.Color.Brown
+            End If
+
+        Next
+
+        y = 0
+        i = 0
+        y = 0
+        q = 0
+
+        ' FRACTALES DE SIERPINSKI
+
+
+
+        For i = 0 To fila
+
+            paranterior = inicio - q
+            pardespues = inicio + q
+            q = q + 1
+
+
+
+            For y = paranterior To pardespues Step 2
+
+                If (DataGridView1(y, i).Value Mod 2 = 0) Then
+
+                    ListBox2.Items.Add("Fila= " & i & " Columna= " & y & " = " & DataGridView1(y, i).Value)
+
+                End If
+            Next
+        Next
+
+
+        Dim sum As Integer
+        Dim filadiag As Integer
+        filadiag = fila - 1
+        sum = 0
+
+        ' SUMA DE DIAGONALES
+
+        ' Recorrer diagonal principal 1+2+3+4...
+
+        If ComboBox1.SelectedItem.ToString() = "Izquierda" Then
+            y = inicio + 1
+
+            For i = 1 To filadiag
+
+                ListBox3.Items.Add(DataGridView1(y, i).Value)
+                DataGridView1.Rows(i).Cells(y).Style.BackColor = Drawing.Color.Brown
+                sum = sum + DataGridView1(y, i).Value
+
+                y = y - 1
+
+            Next
+        End If
+
+        ' Recorrer diagonal principal de manera inversa 10+9+8...
+
+        If ComboBox1.SelectedItem.ToString() = "Derecha" Then
+            y = inicio - 1
+
+            For i = 1 To filadiag
+
+                ListBox3.Items.Add(DataGridView1(y, i).Value)
+                DataGridView1.Rows(i).Cells(y).Style.BackColor = Drawing.Color.Red
+                sum = sum + DataGridView1(y, i).Value
+                y = y + 1
+
+            Next
+
+        End If
+        Label5.Text = sum
+
+        'Catch ex As Exception
+        '    MsgBox("Error  " & ex.Message)
+
+        'End Try
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Close()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        DataGridView1.Columns.Clear()
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+        ListBox3.Items.Clear()
+        TextBox1.Clear()
+        Label5.Text = " "
+        txtfibonaci.Clear()
+
+
+    End Sub
+
+    
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+        Button1.Enabled = False
+       
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57 Or Asc(e.KeyChar) = 8 Or Asc(e.KeyChar) = 13 Then
+
+            If TextBox1.Text <> "" And Asc(e.KeyChar) = 13 Then
+                Button1.Enabled = True
+
+            End If
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtfibonaci_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtfibonaci.KeyPress
+        'If Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57 Or Asc(e.KeyChar) = 8 Or Asc(e.KeyChar) = 13 Then
+
+        '    If txtfibonaci.Text <> "" And Asc(e.KeyChar) = 13 Then
+        '        Button1.Enabled = True
+
+        '    End If
+
+
+        'Else
+        'e.Handled = True
+
+        'End If
+    End Sub
+
+    Private Sub txtfibonaci_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtfibonaci.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
